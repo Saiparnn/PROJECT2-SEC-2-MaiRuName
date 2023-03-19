0a9b789a785bece3caff227b7f1c84883328278b
babyxsorpor <88129083+Saiparnn@users.noreply.github.com>
2023-03-19 23:35:00 +0700

    update movie box

---
+++ src/components/items/MovieList.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <img src="@/assets/images/avengers.jpeg" class="hover:opacity-80 transition easy-in-out duration-150"/>
+    <h3>Avengers</h3>
+    <div>
+      Director | December 6 2022 <br />
+    </div>
+    <span class="text-sm text-gray-500">
+      Action Adventure Fantasy Sci-Fi
+    </span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
