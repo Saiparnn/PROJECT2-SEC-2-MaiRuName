e17ed04534e8ef5ed2e3f42b99926eec80039630
babyxsorpor <88129083+Saiparnn@users.noreply.github.com>
2023-03-19 23:36:49 +0700

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import Navbar from "@/components/header/Navbar.vue";
+import MovieBox from "@/components/MovieBox.vue";
+</script>
+
+<template>
+  <div id="app">
+    <Navbar />
+    <MovieBox />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
