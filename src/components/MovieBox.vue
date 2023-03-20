9ce193a1dedf7049a79f581f832917a331186fe3
babyxsorpor <88129083+Saiparnn@users.noreply.github.com>
2023-03-20 23:56:15 +0700

    Add files via upload

--- src/components/MovieBox.vue
+++ src/components/MovieBox.vue
@@ -1,17 +1,12 @@
 <script setup>
-  import MovieList from "@/components/items/MovieList.vue";
+  import MovieList from "@/components/items/MovieList.vue"
+
 </script>
 
 <template>
   <div class="mx-5">
 <!--    <h2 class="mt-5 ml-5 uppercase text-lg font-semibold">All Movies</h2>-->
     <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-5 gap-8 mt-9 ml-10 mr-10">
-      <MovieList />
-      <MovieList />
-      <MovieList />
-      <MovieList />
-      <MovieList />
-      <MovieList />
       <MovieList />
     </div>
   </div>
