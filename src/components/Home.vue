32f92e0f18d875bc5193ee1aad9072dbe1e062d1
babyxsorpor <88129083+Saiparnn@users.noreply.github.com>
2023-03-25 23:25:39 +0700

    Add files via upload

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,10 @@
 <script setup>
 import Navbar from "@/components/header/Navbar.vue";
-import MovieBox from "@/components/MovieBox.vue";
 </script>
 
 <template>
   <div id="app" class="bg-[#659DBD] pb-10">
     <Navbar />
-<!--    <MovieBox />-->
   </div>
 </template>
 
