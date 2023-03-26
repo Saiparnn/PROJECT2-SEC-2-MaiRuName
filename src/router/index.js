187041b719354fb97f8932d4f3fa2eaacfdeb2e7
babyxsorpor <88129083+Saiparnn@users.noreply.github.com>
2023-03-26 22:14:37 +0700

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-// import Home from '../App.vue'
-// import Login from '../components/Login.vue'
+import { createRouter, createWebHistory} from 'vue-router'
 
-// const routes = [
-//     {
-//         path: '/',
-//         component: Home
-//     },
-//     {
-//         path: '/login',
-//         component: Login
-//     } 
-// ]
+const routes = [
+    {
+        path : '/',
+        component : () => import('../components/MovieBox.vue')
+    },
+    {
+        path : '/navbar',
+        component : () => import('../components/header/Navbar.vue')
+    },
+    {
+        path : '/moviebox',
+        component : () => import('../components/MovieBox.vue')
+    },
+    {
+        path: '/update/:id',
+        name: 'update',
+        component: () => import('../components/UpdateMovie.vue')
+    },
+    {
+        path: '/movie/:id',
+        name: 'movie',
+        component: () => import('../components/EachMovie.vue')
+    }
+]
 
-// const router = createRouter( {
-//     history: createWebHistory(),
-//     routes
-// })
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
 
-// export default router
+export default router
